6fcc464e981e0753e9c28db12ffc6477b0950e00
Syahrizal Setiawan <syahrizal.setiawan11@gmail.com>
2020-12-07 20:02:18 +0700

    initial commit

---
+++ src/components/navigation.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Avatar } from 'antd'
+
+const navLinks = [
+  {
+    title: 'Home',
+    path: '/'
+  },
+  {
+    title: 'Blog',
+    path: '/blog'
+  },
+  {
+    title: 'Contact',
+    path: '/contact-us'
+  },
+  {
+    title: 'Login',
+    path: '/login'
+  }
+]
+
+
+export default function Navigation ({ user }) {
+  const [menuActive, setMenuActive] = useState(false)
+  return (
+    <nav className="site-navigation">
+      <span className="menu-title">Blog App</span>
+      <div className={`menu-content-container ${menuActive && 'active'}`}>
+        <ul>
+          {navLinks.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link to={item.path}>{item.title}</Link>
+              </li>
+            )
+          })}
+        </ul>
+        <span className="menu-avatar-container">
+          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size={36} />
+          <span className="menu-avatar-name">{`${user.firstName} ${user.lastName}`}</span>
+        </span>
+      </div>
+      <i className={`ionicons icon ion-ios-${!menuActive ? 'menu' : 'close'}`} onClick={() => setMenuActive(!menuActive)} />
+    </nav>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/styles/base.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css'
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
 
 ReactDOM.render(
   <React.StrictMode>
